47489bd92d2d4b938b6b1f859a13d273ec7cb0f6
Robertas <robertasqq@gmail.com>
2023-06-29 10:45:36 +0300

    gitignore modified, basic functionality templates added

--- collectibles_database/collectibles_db_app/urls.py
+++ collectibles_database/collectibles_db_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('collectibles_list/', views.CollectiblesListView.as_view(), name='collectibles_list'),
 ]
--- collectibles_database/collectibles_db_app/views.py
+++ collectibles_database/collectibles_db_app/views.py
@@ -5,6 +5,10 @@ from django.views.generic import ListView
 from . import models
 
 
+def index(request):
+    return render(request, 'collectibles_database/index.html')
+
+
 class CollectiblesListView(LoginRequiredMixin, ListView):
     model = models.CollectibleItem
     template_name = 'collectibles_database/collectibles_list.html'
