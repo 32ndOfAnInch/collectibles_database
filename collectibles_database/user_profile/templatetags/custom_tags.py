826cd3fca593d841de3bd57b169f7dd4be2f4709
Robertas <robertasqq@gmail.com>
2023-07-12 16:05:54 +0300

    notifications work. Need some more testing though

--- collectibles_database/user_profile/templatetags/custom_tags.py
+++ collectibles_database/user_profile/templatetags/custom_tags.py
@@ -7,3 +7,8 @@ register = template.Library()
 def is_friend(user, friend_id):
     profile = Profile.objects.get(user=user) 
     return profile.friends.filter(id=friend_id).exists()
+
+
+@register.filter
+def has_pending_friend_requests(user):
+    return FriendRequest.objects.filter(receiver=user, status=1).exists()
